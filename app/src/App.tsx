2fb238cada6d70b150d31674ab1156a359b3862e
yantavares <yantdo1@gmail.com>
2023-09-14 15:42:31 -0300

    added first page template

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
+import garfield from "./assets/garfield.jpeg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Paineis do Carmo :3</h1>
-      <h2>Counter</h2>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "50%",
+        }}
+      >
+        <h1>Paineis do Carmo (Acabamo o PIBIC)</h1>
+        <img src={garfield} alt="garfield with hat" height="400px" />
+        <h2>Qauntas pessoas acharam esse garfield fofo ^ </h2>
+        <h3>{count}</h3>
+        <button style={{ width: "15%" }} onClick={() => setCount(count + 1)}>
+          Eu!!!
+        </button>
+      </div>
     </>
   );
 }
