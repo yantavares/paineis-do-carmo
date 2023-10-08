5fe67dddf72c4a6b76ba2083e8d4d784adf1c185
yantavares <yantdo1@gmail.com>
2023-10-08 18:37:02 -0300

    paintingDetail first part done

--- art-gallery-pibic/src/components/PaintingDesciption/index.tsx
+++ art-gallery-pibic/src/components/PaintingDesciption/index.tsx
@@ -1,10 +1,50 @@
+import temp from "../../assets/temp.png";
+import {
+  ChurchName,
+  DescriptionText,
+  PaintingContainer,
+  PaintingName,
+  ImageContainer,
+  TextContainer,
+  TagContainer,
+  Tag,
+  LineDivider,
+  Container,
+  Description,
+  PaintingImage,
+} from "./styles";
+
 export const PaintingDescription = () => {
   return (
-    <div>
-      <div>
-        <h1>PaintingDescription</h1>
-      </div>
-    </div>
+    <>
+      <Container>
+        <PaintingContainer>
+          <TextContainer>
+            <Description>
+              <ChurchName>Test</ChurchName>
+              <PaintingName>Pintura</PaintingName>
+              <DescriptionText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et
+                massa mi. Aliquam in hendrerit urna. Pellentesque sit amet
+                sapien fringilla, mattis ligula consectetur, ultrices mauris.
+                Maecenas vitae mattis tellus.
+              </DescriptionText>
+            </Description>
+
+            <TagContainer>
+              <Tag>Tag1</Tag>
+              <Tag>Tag2</Tag>
+              <Tag>Tag3</Tag>
+            </TagContainer>
+          </TextContainer>
+
+          <ImageContainer>
+            <PaintingImage src={temp} alt="Temporary church" />
+          </ImageContainer>
+        </PaintingContainer>
+        <LineDivider />
+      </Container>
+    </>
   );
 };
 
--- art-gallery-pibic/src/components/PaintingDesciption/styles.ts
+++ art-gallery-pibic/src/components/PaintingDesciption/styles.ts
@@ -3,7 +3,74 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  height: 100%;
+  padding: 0 10%;
+  margin-top: 2rem;
+`;
+
+export const PaintingContainer = styled.div`
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 40%;
+`;
+
+export const ChurchName = styled.h2`
+  border: 1px solid white;
+  border-radius: 2rem;
+  width: max-content;
+  padding: 0 1rem;
+  margin: 0;
+`;
+
+export const PaintingName = styled.h1`
+  margin: 0;
+`;
+
+export const DescriptionText = styled.p`
+  margin: 0;
+`;
+
+export const ImageContainer = styled.div`
+  width: 50%;
+`;
+
+export const TagContainer = styled.div`
+  justify-self: flex-end;
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+`;
+
+export const Tag = styled.p`
+  margin: 0;
+  padding: 0.5rem 1rem;
+  border-radius: 2rem;
+  background-color: #000000;
+  color: white;
+`;
+
+export const LineDivider = styled.div`
   width: 100%;
-  padding: 0 20px;
-  margin-top: 20px;
+  height: 1px;
+  background-color: rgba(233, 236, 239, 0.5);
+  margin: 2rem 0;
+`;
+
+export const Description = styled.p`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  text-align: justify;
+`;
+
+export const PaintingImage = styled.img`
+  width: 100%;
+  border-radius: 2rem;
 `;
