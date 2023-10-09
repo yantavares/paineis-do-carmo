bafbe49dd29eac87cad2a72d757512f4af840ed5
yantavares <yantdo1@gmail.com>
2023-10-09 17:31:39 -0300

    added footer and header templates

--- art-gallery-pibic/src/App.tsx
+++ art-gallery-pibic/src/App.tsx
@@ -1,10 +1,14 @@
 import PaintingDetail from "./pages/PaintingDetail";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <PaintingDetail />
+      <Footer />
     </div>
   );
 }
