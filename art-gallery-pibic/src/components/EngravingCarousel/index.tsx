1b5370425e0c09e25640bd6524805f2a8b26b8fa
Yan Tavares <yantdo1@gmail.com>
2023-10-09 09:29:16 -0300

    carousel done

--- art-gallery-pibic/src/components/EngravingCarousel/index.tsx
+++ art-gallery-pibic/src/components/EngravingCarousel/index.tsx
@@ -1,6 +1,7 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import temp from "../../assets/temp.png";
+
 const responsive = {
   superLargeDesktop: {
     breakpoint: { max: 4000, min: 3000 },
@@ -22,69 +23,93 @@ const responsive = {
 
 const EngravingCarousel = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
+    <Carousel
+      arrows={false}
+      swipeable={true}
+      autoPlay={true}
+      autoPlaySpeed={2000}
+      infinite={true}
+      responsive={responsive}
+      showDots={true}
+      focusOnSelect={true}
+      centerMode={true}
+      transitionDuration={2000}
+      customTransition="transform 2000ms ease-in-out"
     >
-      <Carousel
-        autoPlay={true}
-        autoPlaySpeed={3000}
-        infinite={true}
-        responsive={responsive}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1rem",
+          marginBottom: "3rem",
+          userSelect: "none",
+          pointerEvents: "none",
+        }}
       >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "1rem",
-          }}
-        >
-          <img src={temp} style={{ maxWidth: "90%" }} alt="" />
-          Item 1
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "1rem",
-          }}
-        >
-          <img src={temp} style={{ maxWidth: "90%" }} alt="" />
-          Item 2
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "1rem",
-          }}
-        >
-          <img src={temp} style={{ maxWidth: "90%" }} alt="" />
-          Item 3
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "1rem",
-          }}
-        >
-          <img src={temp} style={{ maxWidth: "90%" }} alt="" />
-          Item 4
-        </div>
-      </Carousel>
-    </div>
+        <img
+          src={temp}
+          style={{ maxWidth: "90%", userSelect: "none", pointerEvents: "none" }}
+          alt=""
+        />
+        Item 1
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1rem",
+          userSelect: "none",
+          pointerEvents: "none",
+        }}
+      >
+        <img
+          src={temp}
+          style={{ maxWidth: "90%", userSelect: "none", pointerEvents: "none" }}
+          alt=""
+        />
+        Item 2
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1rem",
+          userSelect: "none",
+          pointerEvents: "none",
+        }}
+      >
+        <img
+          src={temp}
+          style={{ maxWidth: "90%", userSelect: "none", pointerEvents: "none" }}
+          alt=""
+        />
+        Item 3
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1rem",
+          userSelect: "none",
+          pointerEvents: "none",
+        }}
+      >
+        <img
+          src={temp}
+          style={{ maxWidth: "90%", userSelect: "none", pointerEvents: "none" }}
+          alt=""
+        />
+        Item 4
+      </div>
+    </Carousel>
   );
 };
 
