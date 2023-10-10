25c594e3741bef8406b1f5d2b118cb815f74135e
yantavares <yantdo1@gmail.com>
2023-10-10 16:36:02 -0300

    fixed author and church

--- art-gallery-pibic/src/pages/PaintingDetail/index.tsx
+++ art-gallery-pibic/src/pages/PaintingDetail/index.tsx
@@ -6,6 +6,7 @@ import teresa from "../../assets/teresa.jpeg";
 const painting1 = {
   title: "Visão: a monja Teresa é protegida por Jesus",
   author: "João de Deus e Sepúlveda (Séc. XVIII)",
+  church: "Igreja de Santa Teresa",
   tags: ["Teresa", "Jesus", "Pena", "Anjo", "Espada"],
   description:
     "Vi-me, estando em oração, sozinha num grande campo, e em redor de mim muita gente de modos diversos que me cercava. Todos, me parece, tinham armas nas mãos para me agredir: uns, lanças; outros, espadas; outros, adagas e outros, estoques muito compridos; enfim, eu não podia sair por nenhum lado sem me pôr em perigo de morte, e só, sem ninguém que se achasse do meu lado. Estando meu espírito nesta aflição, que não sabia que fazer de mim, levantei os olhos ao Céu e vi Cristo, não no Céu, mas bem por cima de mim, no ar, e me estendia a mão e de ali mesmo me favorecia, de maneira que eu não temia toda aquela outra gente; nem eles, embora quisessem, me podiam fazer dano",
@@ -19,8 +20,9 @@ const PaintingDetail = () => {
         title={painting1.title}
         desciption={painting1.description}
         tags={painting1.tags}
-        church={painting1.author}
+        church={painting1.church}
         image={painting1.image}
+        author={painting1.author}
       />
       <Engravings />
     </DetailContainer>
