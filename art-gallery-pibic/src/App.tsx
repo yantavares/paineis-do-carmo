056e72782b53d76932c6581411c2dd2030e851f2
yantavares <yantdo1@gmail.com>
2024-02-06 18:31:37 -0300

    Add museum logo and center title

--- art-gallery-pibic/src/App.tsx
+++ art-gallery-pibic/src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import React from "react";
+import Icon from "./assets/baroque-no-bg.png";
 
 function App() {
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        height: "8rem",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img height={"100%"} src={Icon} alt="Museu Barroco" />
       <h1>Museu Barroco</h1>
-    </>
+    </div>
   );
 }
 
