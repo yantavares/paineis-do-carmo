c19b13748238c31bf325ecde41fc59794d46e216
yantavares <yantdo1@gmail.com>
2024-02-06 18:08:22 -0300

    deleted old proj

--- art-gallery-pibic/src/pages/PaintingDetail/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const DetailContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 0 8%;
-`;
--- art-gallery-pibic/src/utils/stringOperations.ts
+++
@@ -1,5 +0,0 @@
-export function capitalizeStr(str: string) {
-  const newStr = str.charAt(0).toUpperCase() + str.slice(1);
-
-  return newStr;
-}
