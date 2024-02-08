b970b4f711091d83c4f2b608f089284211ba7126
yantavares <yantdo1@gmail.com>
2024-02-08 18:45:40 -0300

    Add Header component and update styles

---
+++ art-gallery-pibic/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "src/components/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      Home
+    </div>
+  );
+};
+export default Home;
