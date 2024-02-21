056f261ea066ee32dc9c825b0f14cd0841523007
yantavares <yantdo1@gmail.com>
2024-02-20 22:14:29 -0300

    Refactor ImageCarousel and RoundBox components

--- art-gallery-pibic/src/pages/Home/RoundBox/index.tsx
+++ art-gallery-pibic/src/pages/Home/RoundBox/index.tsx
@@ -1,12 +1,37 @@
-import React from "react";
-import { BoxButton, BoxText, RoundBoxContainer } from "./styles";
+// Import useState from React
+import React, { useState } from "react";
+import {
+  BoxButton,
+  BoxText,
+  DropdownContent,
+  RoundBoxContainer,
+} from "./styles";
 
 const RoundBox = ({ text, buttonText, color }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <RoundBoxContainer color={color}>
-      <BoxText>{text}</BoxText>
-      <BoxButton>{buttonText}</BoxButton>
-    </RoundBoxContainer>
+    <>
+      <RoundBoxContainer color={color} $isopen={isOpen.toString()}>
+        <BoxText>{text}</BoxText>
+        <BoxButton onClick={toggleDropdown}>{buttonText}</BoxButton>
+      </RoundBoxContainer>
+
+      <DropdownContent color={color} $isopen={isOpen.toString()}>
+        {/* <iframe
+          width="100%"
+          height="300"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          title="YouTube video player"
+        /> */}
+        <p>Some additional text here</p>
+      </DropdownContent>
+    </>
   );
 };
+
 export default RoundBox;
--- art-gallery-pibic/src/pages/Home/RoundBox/styles.ts
+++ art-gallery-pibic/src/pages/Home/RoundBox/styles.ts
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 
-export const RoundBoxContainer = styled.div`
+interface RoundBoxProps {
+  color: string;
+  $isopen: string;
+}
+
+export const RoundBoxContainer = styled.div<RoundBoxProps>`
   background-color: ${(props) => props.color};
-  border-radius: 1rem;
+  border-radius: ${(props) =>
+    props.$isopen === "true" ? "1rem 1rem 0 0" : "1rem"};
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 2rem;
-  border-radius: 1rem;
+  transition: all 0.5s;
 `;
 
 export const BoxText = styled.h2`
@@ -24,4 +30,20 @@ export const BoxButton = styled.button`
     transform: scale(1.04);
     background-color: rgba(255, 255, 255, 0.15);
   }
+`;
+
+interface DropdownContentProps {
+  color: string;
+  $isopen: string;
+}
+
+export const DropdownContent = styled.div<DropdownContentProps>`
+  background-color: ${(props) => props.color};
+  transition: max-height 0.5s ease-in-out, opacity 0.5s ease-in-out;
+  max-height: ${(props) => (props.$isopen === "true" ? "500px" : "0")};
+  opacity: ${(props) => (props.$isopen === "true" ? "1" : "0")};
+  overflow: hidden;
+  color: white;
+  padding: 1rem;
+  border-radius: 0 0 1rem 1rem;
 `;
