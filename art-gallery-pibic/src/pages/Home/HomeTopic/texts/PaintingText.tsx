9de04b34e00c6443d369b6f08caf861b20d9f42a
yantavares <yantdo1@gmail.com>
2024-03-01 20:05:27 -0300

    Add PaintingText component and its styles

---
+++ art-gallery-pibic/src/pages/Home/HomeTopic/texts/PaintingText.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { DataTitle, DataInfoContainer, DataInfo, PaintingDate } from "./styles";
+import { Painting } from "../../mockData";
+
+const PaintingText = ({ painting }: { painting: Painting }) => {
+  return (
+    <>
+      {painting.date ? (
+        <DataTitle>
+          {painting.title} <PaintingDate>({painting.date})</PaintingDate>
+        </DataTitle>
+      ) : (
+        <DataTitle>{painting.title}</DataTitle>
+      )}
+
+      <DataInfoContainer>
+        {painting.city && <DataInfo>{painting.city}</DataInfo>}
+        {painting.state && painting.city ? (
+          <DataInfo>, {painting.state}</DataInfo>
+        ) : (
+          <DataInfo>{painting.state}</DataInfo>
+        )}
+      </DataInfoContainer>
+    </>
+  );
+};
+export default PaintingText;
