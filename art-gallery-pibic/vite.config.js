76392e26f5c8e9bd6c6c1f3bbc75159d3025ad1f
yantavares <yantdo1@gmail.com>
2024-03-01 20:47:03 -0300

    Update deploy-preview.yml and vite.config.js

--- art-gallery-pibic/vite.config.js
+++ art-gallery-pibic/vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/art-gallery-pibic/",
   plugins: [react()],
   resolve: {
     alias: {
