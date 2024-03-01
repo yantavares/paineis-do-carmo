9e0ac1aa3dde3c384b6c579b8f6f427a19686e8b
yantavares <yantdo1@gmail.com>
2024-03-01 20:05:17 -0300

    Add HomeTopic component and its styles

---
+++ art-gallery-pibic/src/pages/Home/HomeTopic/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const SeeMoreButton = styled.button`
+  display: flex;
+  gap: 0.8rem;
+  align-items: center;
+  border: none;
+  margin: 0;
+  margin-bottom: 1rem;
+  padding: 0;
+`;
+
+export const DataContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Data = styled.div`
+  width: 18%;
+`;
+
+export const DataImage = styled.img`
+  max-width: 14rem;
+  border-radius: 0.8rem;
+`;
