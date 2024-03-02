76e46fdd570be700dc3c6b331ec94b3ef6a6fe24
yantavares <yantdo1@gmail.com>
2024-03-01 21:18:21 -0300

    Add base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/paineis-do-carmo/",
   plugins: [react()],
   resolve: {
     alias: {
