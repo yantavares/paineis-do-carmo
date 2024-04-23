a84048cc4f0952961b94ccff2132ba29cfcb9c1c
yantavares <yantdo1@gmail.com>
2024-04-23 16:34:45 -0300

    Add click functionality to Tag component in Tags/index.tsx

--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Tag, TagsContainer } from "./styles";
+import { useNavigate } from "react-router-dom";
 
 interface TagsProps {
   tags: string[] | undefined;
 }
 
 const Tags = ({ tags }: TagsProps) => {
+  const navigate = useNavigate();
   return (
     <TagsContainer>
-      {tags && tags.map((tag, index) => <Tag key={index}>{tag}</Tag>)}
+      {tags &&
+        tags.map((tag, index) => (
+          <Tag onClick={() => navigate(`/topicos/${tag}`)} key={index}>
+            {tag}
+          </Tag>
+        ))}
     </TagsContainer>
   );
 };
