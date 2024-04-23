01b54c39f7d448cb17aacfa8c6ba0cf6fddb9d05
yantavares <yantdo1@gmail.com>
2024-04-23 11:15:15 -0300

    Update border color on hover in Tag component

--- src/components/Tags/styles.ts
+++ src/components/Tags/styles.ts
@@ -10,6 +10,7 @@ export const Tag = styled.span`
   border: 1px solid #ddd;
   transition: all 0.3s;
   &:hover {
+    border-color: ${colors.green};
     transform: scale(1.05);
     cursor: pointer;
   }
