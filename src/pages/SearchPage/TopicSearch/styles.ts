90824080c9e5a949caf40f96fc4c0023e466bd75
yantavares <yantdo1@gmail.com>
2025-04-28 22:01:56 -0300

    Refactor BigTag and BigTagMobile styles for improved layout consistency

--- src/pages/SearchPage/TopicSearch/styles.ts
+++ src/pages/SearchPage/TopicSearch/styles.ts
@@ -9,6 +9,8 @@ export const BigTag = styled.span`
   padding: 1.25rem 1.5rem;
   border: 1px solid #ddd;
   transition: all 0.3s;
+  flex: 1 1 auto;
+  text-align: center;
   &:hover {
     border-color: ${colors.mainColor};
     transform: scale(1.1);
@@ -20,9 +22,11 @@ export const BigTagMobile = styled.span`
   background-color: ${colors.white};
   border-radius: 2rem;
   color: #333;
-  font-size: 2rem;
+  flex: 1 1 auto;
+  font-size: 1.8rem;
   padding: 1.25rem 1.5rem;
   border: 1px solid #ddd;
+  text-align: center;
   transition: all 0.3s;
   &:hover {
     border-color: ${colors.mainColor};
