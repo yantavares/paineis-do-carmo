c2f7a107c1bd0bbb74aead558a94aa88418b2ab9
yantavares <yantdo1@gmail.com>
2025-09-25 20:05:25 -0300

    Update BigTag and BigTagMobile styles: change background color to white and increase border thickness

--- src/pages/SearchPage/TopicSearch/styles.ts
+++ src/pages/SearchPage/TopicSearch/styles.ts
@@ -2,12 +2,13 @@ import styled from "styled-components";
 import colors from "src/utils/colors";
 
 export const BigTag = styled.span`
-  background-color: ${colors.white};
+  background-color: #fff;
   border-radius: 2rem;
   color: #333;
   font-size: 2.5rem;
   padding: 1.25rem 1.5rem;
-  border: 1px solid #ddd;
+  border: 2px solid #ddd;
+  max-width: 45rem;
   transition: all 0.3s;
   flex: 1 1 auto;
   text-align: center;
@@ -19,13 +20,14 @@ export const BigTag = styled.span`
 `;
 
 export const BigTagMobile = styled.span`
-  background-color: ${colors.white};
+  background-color: #fff;
   border-radius: 2rem;
   color: #333;
   flex: 1 1 auto;
+  max-width: 80%;
   font-size: 1.8rem;
   padding: 1.25rem 1.5rem;
-  border: 1px solid #ddd;
+  border: 2px solid #ddd;
   text-align: center;
   transition: all 0.3s;
   &:hover {
